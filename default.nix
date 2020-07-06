ac76dd1d873560fff2d24254114d4475912a4084
Arian van Putten <arian.vanputten@gmail.com>
2020-07-06 11:34:40 +0200

    Add CI using Github Actions

---
+++ default.nix
@@ -0,0 +1,31 @@
+let
+  nixpkgs = (import ./nix/sources.nix).nixpkgs;
+  pkgs = import nixpkgs {};
+
+  env = pkgs.bundlerEnv {
+    name = "jekyll-env";
+    ruby = pkgs.ruby;
+    gemfile = ./Gemfile;
+    lockfile = ./Gemfile.lock;
+    gemset = ./gemset.nix;
+  };
+in
+{
+  website = pkgs.stdenv.mkDerivation {
+    name = "jekyll";
+    src = pkgs.nix-gitignore.gitignoreSource [] ./.;
+    nativeBuildInputs = [ env pkgs.bundler pkgs.ruby ];
+    LC_ALL = "C.UTF-8";
+    buildPhase = ''
+      jekyll build --trace
+    '';
+    installPhase = ''
+      cp -r _site $out
+    '';
+  };
+  update-dependencies = pkgs.writeScript "update-dependencies"
+    ''
+      ${pkgs.niv}/bin/niv update
+      ${pkgs.bundix}/bin/bundix -l bundix
+    '';
+}
